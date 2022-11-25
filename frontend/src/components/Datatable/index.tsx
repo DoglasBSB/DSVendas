5ec3251e67c783af81d99cf034fce471b3554480
Dôglas <doglas_bsb@hotmail.com>
2022-11-25 15:11:24 -0300

    alteração de texto para teste de regressão visual utilizando percy

--- frontend/src/components/Datatable/index.tsx
+++ frontend/src/components/Datatable/index.tsx
@@ -38,7 +38,7 @@ const DataTable = () => {
                         <th>Data</th>
                         <th>Vendedor</th>
                         <th>Clientes visitados</th>
-                        <th>Negocios fechados</th>
+                        <th>Negócios fechados</th>
                         <th>Valor</th>
                     </tr>
                 </thead>
